11ef52e705a161dbaa20c1b5aaed1bb694aad98c
Kai Dizon <Kai@Emilynns-MacBook-Pro.local>
2026-02-03 09:43:33 -0800

    Completed CL3

--- Module01_Essentials/CL3.py
+++ Module01_Essentials/CL3.py
@@ -7,12 +7,23 @@
 NL = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 ## Write a list comprehension to collect the diagonal items from matrix NL
 # ** your code **
+diagonal = [NL[i][i] for i in range(len(NL))]
 
+print(diagonal)
 ##### CL3.2: Using my_list, create a list of tuples where tuple is (n, n**3) where n is each number in the list.
 my_list = [1, 2, 3, 4, 5]
 # ** your code **
+result = [(n, n**3) for n in my_list]
 
+print(result)
 
 ##### CL3.3: Multiple all the values of the items in a dictionary 
 num_dict = {'num1':100, 'num2':346, 'num3':-24}
 # ** your code **
+
+product = 1
+for value in num_dict.values():
+	product *= value
+
+print(product)
+
